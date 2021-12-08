b1309b6098c1b9d4caa50e4a237d0d59f83bf952
Zejakob123 <78012191+Zejakob123@users.noreply.github.com>
2021-12-08 13:51:17 +0800

    Add files via upload

---
+++ Lab3/G03-L1-75405/success.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            body{
+                background-color: #CCD5AE;
+            }
+
+            main{
+                text-align: center;
+            }
+
+            a{
+                text-decoration: none;
+                color: white;
+                padding: 16px 50px;
+                border-radius: 10px;
+                background-color: #293241;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <main>
+            <img src="source/icon/success.png" alt="Success Icon">
+            <h1>Registration Success</h1>
+            <a href="registration.html">Go back</a>
+        </main>
+    </body>
+</html>
